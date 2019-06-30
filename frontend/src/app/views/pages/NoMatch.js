67cf17f704b4dd9d66d5b05392b4a87e50c1e029
Fernando Alegría <fernanalegria@gmail.com>
2019-06-30 14:25:47 +0200

    fix: Fix the layout of error pages and keep the navigation bar on top

--- frontend/src/app/views/pages/NoMatch.js
+++ frontend/src/app/views/pages/NoMatch.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import BaseContainer from '../common/BaseContainer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Row from 'react-bootstrap/Row';
+import { Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { rootUrl } from '../../../index';
 
@@ -10,17 +10,22 @@ import { rootUrl } from '../../../index';
  */
 const NoMatch = () => (
   <BaseContainer align="top">
-    <h1 className="text-center">
-      <FontAwesomeIcon icon="exclamation-triangle" /> 404 Not Found
-    </h1>
-    <h5 className="text-center my-4">
-      Sorry, the requested page could not be found
-    </h5>
-    <Row>
-      <Link to={`${rootUrl}/welcome`} className="btn btn-secondary btn-sm mx-auto">
-        Go back to Home page
-      </Link>
-    </Row>
+    <Col lg="6" md="8" className="m-auto">
+      <h1 className="text-center">
+        <FontAwesomeIcon icon="exclamation-triangle" /> 404 Not Found
+      </h1>
+      <h5 className="text-center my-4">
+        Sorry, the requested page could not be found
+      </h5>
+      <Row>
+        <Link
+          to={`${rootUrl}/welcome`}
+          className="btn btn-secondary btn-sm mx-auto"
+        >
+          Go back to Home page
+        </Link>
+      </Row>
+    </Col>
   </BaseContainer>
 );
 
